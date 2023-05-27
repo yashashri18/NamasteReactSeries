656f3d897a313e7f430fc71dcf0282f6c760e2ac
yashashri.bongulwar <yashashri.bongulwar@michelin.com>
2023-05-27 14:10:28 +0530

    episode 3 code

--- Episode 02 - Iginite the App/Code/script.js
+++ Episode 02 - Iginite the App/Code/script.js
@@ -18,6 +18,7 @@ const heading1 = React.createElement(
     "h1",
     {
         id: "title1",
+        key:"h1",
     },
     "Heading 1"
 )
@@ -25,6 +26,7 @@ const heading2 = React.createElement(
     "h2",
     {
         id: "title2",
+        key: "h2",
     },
     "Heading 2"
 )
--- Episode 03 - Laying the Foundation/Code/script.js
+++ Episode 03 - Laying the Foundation/Code/script.js
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDOM  from "react-dom/client"
 
+//React.createElement -> Object -> Html(DOM)
 const heading = React.createElement(
     "h1",
     {
@@ -9,33 +10,51 @@ const heading = React.createElement(
     },
     "Hello World from React"
 )
-console.log(heading) //returns object
+//console.log(heading) //returns object
 //React element at the end of the day is JS object
 
+//Writing React.createElement is painfull if we want to cretae huge html
+//hence jsx comes into picture 
+//we write jsx and babel converts jsx into React.createElement
+//JSX is not html , it is html like syntax 
 
-//nested element
-const heading1 = React.createElement(
-    "h1",
-    {
-        id: "title1",
-    },
-    "Heading 1"
+//writing jsx in single line
+//JSX -> React.createElement -> Object -> Html(DOM)
+//React Element - 
+const heading1 = <h1 id="heading1"> this is Heading 1</h1>
+
+//React Element - writing in multiple lines
+const heading2 = (
+    <h1 id="heading2"> 
+        this is Heading 2
+    </h1>
 )
-const heading2 = React.createElement(
-    "h2",
-    {
-        id: "title2",
-    },
-    "Heading 2"
+ 
+//React Component
+const Title = () => (
+    <h1>Namaste React Function</h1>
 )
-const container = React.createElement(
-    "div",
-    {
-        id: "container",
-    },
-    [heading1,heading2]
-)
+
+var xyz = 10;
+//React Component 
+const HeaderComponent = () => {
+    return(
+        <div>
+            {/* we can use react element inside compoent */}
+            {heading2}
+            {/* we can write any piece of js inside jsx */}
+            {/* this is very safe and jsx sanitizes it first */}
+            {xyz}
+            {console.log(10)}
+            {/* Component Composistion */}
+            <Title/>
+            <h1>Namaste React</h1>
+            <h2>This is h2 tag</h2>
+            {2+6}
+        </div>
+    )
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-//root.render(heading);
-root.render(container);
+root.render(<HeaderComponent/>);
